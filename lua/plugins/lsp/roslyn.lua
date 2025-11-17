d5dcc193760988eae0c2a7f2bb892e31dafadda9
Crafterbot <>
2025-11-17 11:24:17 -0500

    Roslyn razor fixes

--- lua/plugins/lsp/roslyn.lua
+++ lua/plugins/lsp/roslyn.lua
@@ -1,11 +1,30 @@
+-- Mostly copied from https://github.com/tris203/rzls.nvim?tab=readme-ov-file#example-configuration
 return {
     {
-        "tris203/rzls.nvim",
-    },
-    {
         "seblyng/roslyn.nvim",
+        ft = { "cs", "razor" },
+        dependencies = {
+            {
+                "tris203/rzls.nvim",
+                config = true,
+            },
+        },
         config = function ()
+            local rzls_path = vim.fn.expand("$MASON/packages/rzls/libexec")
+            local cmd = {
+                "roslyn",
+                "--stdio",
+                "--logLevel=Information",
+                "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+                "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
+                "--razorDesignTimePath=" .. vim.fs.joinpath(rzls_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
+                "--extension",
+                vim.fs.joinpath(rzls_path, "RazorExtension", "Microsoft.VisualStudioCode.RazorExtension.dll"),
+            }
+
             vim.lsp.config("roslyn", {
+                cmd = cmd,
+                handlers = require("rzls.roslyn_handlers"),
                 settings = {
                     ["csharp|background_analysis"] = {
                         dotnet_analyzer_diagnostics_scope = "none",
@@ -14,9 +33,18 @@ return {
                 },
             })
 
-            vim.cmd("autocmd BufNewFile,BufRead *.cshtml set filetype=html.cshtml.razor")
-            vim.cmd("autocmd BufNewFile,BufRead *.razor set filetype=html.cshtml.razor")
-        end
+            -- vim.cmd("autocmd BufNewFile,BufRead *.cshtml set filetype=html.cshtml.razor")
+            -- vim.cmd("autocmd BufNewFile,BufRead *.razor set filetype=html.cshtml.razor")
+        end,
+
+        init = function()
+            vim.filetype.add({
+                extension = {
+                    razor = "razor",
+                    cshtml = "razor",
+                },
+            })
+        end,
     },
 
     -- https://github.com/khoido2003/roslyn-filewatch.nvim
